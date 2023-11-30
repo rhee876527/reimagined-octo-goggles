ba551b55e4aaf2f2d92b55c2ac9c3a3c8c009378
tankaras <26869897+tankaras@users.noreply.github.com>
2023-11-30 11:23:03 +0300

    cfs-zen-tweaks

--- system-packages.nix
+++ system-packages.nix
@@ -2,6 +2,7 @@
 {
 # Add/Make Changes to systemPackages
   environment.systemPackages = with pkgs; [
+    cfs-zen-tweaks # zen tweaks
     wireguard-tools
     unstable.pipewire # latest audio
     unstable.sof-firmware
