e8b9f5c184991f217125295cf8b0a8fe533f0848
tankaras <26869897+tankaras@users.noreply.github.com>
2023-12-01 21:42:18 +0300

    drop cfs-tweaks since we are running custom kernel

--- system-packages.nix
+++ system-packages.nix
@@ -2,7 +2,7 @@
 {
 # Add/Make Changes to systemPackages
   environment.systemPackages = with pkgs; [
-    cfs-zen-tweaks # zen tweaks
+#    cfs-zen-tweaks # zen tweaks for use only with stock kernel
     wireguard-tools
     unstable.pipewire # latest audio
     unstable.sof-firmware
