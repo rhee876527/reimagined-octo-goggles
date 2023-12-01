ccd47945c3534f70c5f2518164fa370835344e19
tankaras <26869897+tankaras@users.noreply.github.com>
2023-12-01 16:34:34 +0300

    more system tuning

--- sysctl.nix
+++ sysctl.nix
@@ -5,5 +5,6 @@
   boot.kernel.sysctl."vm.oom_kill_allocating_task" = lib.mkOverride 500 1;
   boot.kernel.sysctl."kernel.sysrq" = lib.mkOverride 500 0;
   boot.kernel.sysctl."kernel.nmi_watchdog" = lib.mkOverride 500 0;
-  boot.kernel.sysctl."vm.swappiness" = lib.mkOverride 500 20;
+  boot.kernel.sysctl."vm.swappiness" = lib.mkOverride 500 10;
+  boot.kernel.sysctl."vm.max_map_count" = lib.mkOverride 500 2147483642;
 }
