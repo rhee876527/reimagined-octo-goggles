394bbcccb529f400e22d2452083dc2ba00cbfe41
tankaras <26869897+tankaras@users.noreply.github.com>
2023-12-01 16:52:46 +0300

    set correct limit

--- sysctl.nix
+++ sysctl.nix
@@ -6,5 +6,5 @@
   boot.kernel.sysctl."kernel.sysrq" = lib.mkOverride 500 0;
   boot.kernel.sysctl."kernel.nmi_watchdog" = lib.mkOverride 500 0;
   boot.kernel.sysctl."vm.swappiness" = lib.mkOverride 500 10;
-  boot.kernel.sysctl."vm.max_map_count" = lib.mkOverride 500 2147483642;
+  boot.kernel.sysctl."vm.max_map_count" = lib.mkOverride 500 1048576;
 }
